65a7c4fe6ff4cf036c9b4b0600485411c962b916
a_v__month <2365800269@qq.com>
2020-08-10 20:26:44 +0800

    8月10日

--- src/main/java/com/user/UserController.java
+++ src/main/java/com/user/UserController.java
@@ -1,13 +1,37 @@
 package com.user;
 
+import com.vo.TestProperties;
+import org.apache.catalina.User;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+    public static final Logger LOGGER= (Logger) LoggerFactory.getLogger(UserController.class);
+    @Autowired
+    TestProperties testProperties;
     @RequestMapping("hello")
     public String hello(){
         return "hello";
+    }
+    @RequestMapping("testProperties")
+    public String testProperties(){
+        return "Name"+testProperties.getName();
+    }
+    @RequestMapping("Logger")
+    public String logger(){
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "";
     }
 }
---
+++ src/main/java/com/vo/TestProperties.java
@@ -0,0 +1,48 @@
+package com.vo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.net.SocketTimeoutException;
+
+
+@Component
+@PropertySource("classpath:applicationText.properties")
+@ConfigurationProperties(prefix = "user")
+public class TestProperties {
+    public String name;
+    public Integer age;
+    public String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+   /* public static void main(String[] args) {
+        @Autowired
+        TestProperties testProperties;
+        System.out.println(testProperties.getName());
+    }*/
+}
