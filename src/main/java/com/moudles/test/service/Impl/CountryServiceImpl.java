6c3a341e7c355651a4bd910582c8945fe60b634d
a_v__month <2365800269@qq.com>
2020-08-11 19:54:50 +0800

    8月11

--- src/main/java/com/moudles/test/service/Impl/CountryServiceImpl.java
+++ src/main/java/com/moudles/test/service/Impl/CountryServiceImpl.java
@@ -4,9 +4,11 @@ import com.moudles.test.dao.CountryDao;
 import com.moudles.test.model.Country;
 import com.moudles.test.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import javax.xml.ws.soap.Addressing;
 
+@Service
 public class CountryServiceImpl implements CountryService {
     @Autowired
     CountryDao countryDao;
